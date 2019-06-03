5484a82dfde20c42f5277f19aa501d334ba6aaf7
Enis Tocilla <enis@zombiesoup.com>
2019-06-03 04:50:24 +0200

    Implement RiskType model

--- insurance_backend/risk_types/models.py
+++ insurance_backend/risk_types/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class RiskType(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        ordering = ('-created_at', )
+
+    def __str__(self):
+        return 'Risk Type: %s' % (self.name)

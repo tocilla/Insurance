ba33d4270abaf3acf21ca7bd3c933c7a290d2251
Enis Tocilla <enis@zombiesoup.com>
2019-06-03 05:32:12 +0200

    Add models to django admin

--- insurance_backend/risk_types/admin.py
+++ insurance_backend/risk_types/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import RiskType, RiskField, FieldOption
+admin.site.register(RiskType)
+admin.site.register(RiskField)
+admin.site.register(FieldOption)
--- insurance_backend/risks/admin.py
+++ insurance_backend/risks/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Risk, RiskInput
+admin.site.register(Risk)
+admin.site.register(RiskInput)

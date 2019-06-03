4c69839f2efd3c0fc9fe1c788268c2da62873d32
Enis Tocilla <enis@zombiesoup.com>
2019-06-03 05:12:46 +0200

    Implement views for risks

--- insurance_backend/risks/views.py
+++ insurance_backend/risks/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# # Create your views here.
+from rest_framework import viewsets, generics
+# from rest_framework.viewsets import GenericViewSet
+from .models import Risk
+from .serializers import RiskSerializer
 
-# Create your views here.
+
+class RiskViewSet(viewsets.ModelViewSet):
+    queryset = Risk.objects.all()
+    serializer_class = RiskSerializer

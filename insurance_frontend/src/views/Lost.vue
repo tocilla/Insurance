29c3867cb232e80494048aaec4e0bbff6a6b857d
Enis Tocilla <enis@zombiesoup.com>
2019-06-03 13:54:44 +0200

    Add not found page

---
+++ insurance_frontend/src/views/Lost.vue
@@ -0,0 +1,14 @@
+<template>
+    <b-container class="h-100 d-flex justify-content-center align-items-center" id="lost-view">
+        <div>
+            <h1>Page Not Found</h1>
+            <h4>One of us is lost.</h4>
+            <router-link to="/">Take me home.</router-link>
+        </div>
+    </b-container>
+</template>
+<script>
+    export default {
+        name: 'lost',
+    };
+</script>

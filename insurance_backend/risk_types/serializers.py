075942c8edbe0b12f0fb504b14535f02bcd590dc
Enis Tocilla <enis@zombiesoup.com>
2019-06-03 05:00:58 +0200

    Implement RiskTypeSerializer

--- insurance_backend/risk_types/serializers.py
+++ insurance_backend/risk_types/serializers.py
@@ -18,3 +18,30 @@ class RiskFieldSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'type', 'field_options', 'required',)
 
 
+class RiskTypeSerializer(serializers.ModelSerializer):
+    risk_fields = RiskFieldSerializer(many=True, required=True)
+
+    class Meta:
+        model = RiskType
+        fields = ('id', 'name', 'description',
+                  'risk_fields', 'active', 'created_at',)
+        filter_backends = (filters.OrderingFilter,)
+        ordering_fields = ('name', 'created_at')
+
+    @transaction.atomic
+    def create(self, validated_data):
+        """
+        Create and return a new `RiskType` instance, given the validated data.
+        """
+        risk_fields_data = validated_data.pop('risk_fields')
+
+        risk_type = RiskType.objects.create(**validated_data)
+        for risk_field_data in risk_fields_data:
+            field_options_data = risk_field_data.pop('field_options')
+            risk_field = risk_type.risk_fields.create(**risk_field_data)
+
+            for field_option_data in field_options_data:
+                if risk_field_data['type'] is 'option':
+                    risk_field.field_options.create(**field_option_data)
+
+        return risk_type

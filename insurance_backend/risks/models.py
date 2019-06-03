d594f2027478c3e467f4f5f0ff216107f39badbb
Enis Tocilla <enis@zombiesoup.com>
2019-06-03 05:06:17 +0200

    Implement RiskInput model

--- insurance_backend/risks/models.py
+++ insurance_backend/risks/models.py
@@ -16,3 +16,13 @@ class Risk(models.Model):
 
     def __str__(self):
         return 'Risk: %s, Risk Type: %s' % (self.name, self.risk_type)
+
+
+class RiskInput(models.Model):
+    risk = models.ForeignKey(Risk, related_name='risk_inputs', on_delete=models.CASCADE)
+    risk_field = models.ForeignKey(
+        RiskField, related_name='risk_inputs', on_delete=models.CASCADE)
+    value = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return 'Risk Input: %s, Risk: %s' % (self.value, self.risk)

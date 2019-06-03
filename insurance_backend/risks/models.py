8aa208244bc614dc5fb3ef7f62e362412e6b723d
Enis Tocilla <enis@zombiesoup.com>
2019-06-03 05:05:54 +0200

    Implement Risk model

--- insurance_backend/risks/models.py
+++ insurance_backend/risks/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from risk_types.models import RiskType, RiskField
 
 # Create your models here.
+
+
+class Risk(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    risk_type = models.ForeignKey(
+        RiskType, related_name='risks', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    
+    class Meta:
+        ordering = ('-created_at', )
+
+    def __str__(self):
+        return 'Risk: %s, Risk Type: %s' % (self.name, self.risk_type)

9ebffc44edd1707db376a9840d77d11e0411e2ef
Enis Tocilla <enis@zombiesoup.com>
2019-06-03 04:55:15 +0200

    Implement views for risk_types

--- insurance_backend/risk_types/views.py
+++ insurance_backend/risk_types/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
 # Create your views here.
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
+from .models import RiskType
+from .serializers import RiskTypeSerializer
+
+class RiskTypeViewSet(mixins.ListModelMixin,
+                      mixins.CreateModelMixin,
+                      mixins.RetrieveModelMixin,
+                      GenericViewSet):
+    queryset = RiskType.objects.all()
+    serializer_class = RiskTypeSerializer

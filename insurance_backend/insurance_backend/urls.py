0b811cc27d73ef62f10f8f1b02bb5af740ecc207
Enis Tocilla <enis@zombiesoup.com>
2019-06-04 05:07:09 +0200

    Fix coding styles

--- insurance_backend/insurance_backend/urls.py
+++ insurance_backend/insurance_backend/urls.py
@@ -22,19 +22,21 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Insurance API",
-      default_version='v1',
-      contact=openapi.Contact(email="enistocilla@gmail.com"),
-      license=openapi.License(name="MIT License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Insurance API",
+        default_version='v1',
+        contact=openapi.Contact(email="enistocilla@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
-    url(r'^$',schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    url(r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0),
+        name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('auth/', include('rest_auth.urls')),
